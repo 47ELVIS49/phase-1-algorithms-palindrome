d664adc21f1182ab606de54c27edbb9ce161d31b
Elvis Momanyi <elvismomanyi23@gmail.com>
2024-01-16 13:04:46 +0300

    done with the palindrome lab

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
+// index.js
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+function isPalindrome(str) {
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanStr = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  
+  // Check if the cleaned string is the same forwards and backwards
+  return cleanStr === cleanStr.split("").reverse().join("");
 }
 
 module.exports = isPalindrome;
